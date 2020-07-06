f8c6c78a3269caa42c1923a8158bb2050daff29d
Mike Azar <mazar@netflix.com>
2020-07-05 18:12:57 -0700

    Fix error handling and import of user model

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -3,7 +3,8 @@ const {db} = require('../models/User-mod');
 
 const bodyParser=require('body-parser');
 const { nextTick } = require('process');
-const { Mongoose } = require('mongoose');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 const router = express.Router();
 
 
@@ -48,20 +49,27 @@ router.get('/', async(req, res) => {
         await User.find().exec((err, posts) => {
             if (err) return res.json({ "message": err });
             res.json(posts);
-    });
- } catch (err) {
+        });
+    } catch (err) {
+        console.log(`failed with ${err}`)
         res.json({ message: err });
     }
 });
 
 router.post('/',async(req,res)=>{
-    var user = new User(
-        req.body
-    );
-    await user.save((err, user) => {
-        if (err) return res.json({ "message": err });
-        res.json(user);
-    });
+    console.log(`Received user payload ${JSON.stringify(req.body)}`);
+
+    try {
+        var user = new User(req.body);
+        console.log(`User object to save ${JSON.stringify(user)}`)
+        await user.save((err, user) => {
+            if (err) return res.json({ "message": err });
+            res.json(user);
+        });
+    } catch (err) {
+        console.log(`failed with ${err}`)
+        res.json({ "message": JSON.stringify(err) });
+    }
 
 });
 
